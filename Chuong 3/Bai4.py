f45206fc121c0f7c287a4174bbd1a9a1309674d4
nhoangtung <xuan_dpm215547@student.agu.edu.vn>
2025-09-17 17:38:35 +0700

    Thêm bài 3 và 4

---
+++ Chuong 3/Bai4.py
@@ -0,0 +1,16 @@
+#4 :Hãy cho biết kết quả của Boolean Expression
+
+x, y, z = 3, 5, 7
+
+print("a.", x == 3)
+print("b.", x < y)
+print("c.", x >= y)
+print("d.", x <= y)
+print("e.", x != y - 2)
+print("f.", x < 10)
+print("g.", x >= 0 and x < 10)
+print("h.", x < 0 and x < 10)
+print("i.", x >= 0 and x < 2)
+print("j.", x < 0 or x < 10)
+print("k.", x > 0 or x < 10)
+print("l.", x < 0 or x > 10)
